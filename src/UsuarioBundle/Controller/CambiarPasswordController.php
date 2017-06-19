1196064fff7dc171bb407f1ed9d58007fe001f80
Pablo Santiago Semhan <santiagosemhan@gmail.com>
2017-06-19 08:40:35 -0300

    Cambio de contraseña rápido. Visibilidad de la empresa en el listado de personas

--- src/UsuarioBundle/Controller/CambiarPasswordController.php
+++ src/UsuarioBundle/Controller/CambiarPasswordController.php
@@ -10,34 +10,70 @@ use UsuarioBundle\Form\CambiarPasswordType;
 
 class CambiarPasswordController extends Controller
 {
-    public function cambiarPasswordAction(Request $request, Usuario $usuario )
+    public function cambiarPasswordAction(Request $request, Usuario $usuario)
     {
         $form = $this->createForm(CambiarPasswordType::class, $usuario);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $userManager = $this->get('fos_user.user_manager');
-
-	        $dispatcher = $this->get('event_dispatcher');
-
-	        $event = new UsuarioPasswordModificadoEvent($usuario);
-
-	        $dispatcher->dispatch(UsuarioPasswordModificadoEvent::NAME, $event);
-
-            $userManager->updateUser($usuario);
-
-            // set flash messages
-            $this->get('session')->getFlashBag()->add('success', 'La contraseña se ha modificado satisfactoriamente.');
+            $this->changePassword($usuario);
 
             return $this->redirectToRoute('persona_index');
-
         }
-        
+
         return $this->render('UsuarioBundle:CambiarPassword:cambiarPassword.html.twig', array(
             'usuario' => $usuario,
             'form' => $form->createView(),
         ));
     }
 
+    public function cambiarPasswordFormAction(Request $request)
+    {
+        $updated = false;
+
+        $usuario = $this->getUser();
+
+        $form = $this->createForm(CambiarPasswordType::class, $usuario, [
+            'action' => $this->generateUrl('cambiar-password-form'),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->changePassword($usuario);
+
+            $updated = true;
+        }
+
+
+        $formContent = $this->renderView('UsuarioBundle:CambiarPassword:cambiar-password-form.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
+        return $this->json([
+            'content' => $formContent,
+            'updated' => $updated,
+            'valid' => $form->isValid(),
+            'submitted' => $form->isSubmitted(),
+            'errors' => $form->getErrors()
+        ]);
+    }
+
+
+
+    private function changePassword(Usuario $usuario)
+    {
+        $userManager = $this->get('fos_user.user_manager');
+
+        $dispatcher = $this->get('event_dispatcher');
+
+        $event = new UsuarioPasswordModificadoEvent($usuario);
+
+        $dispatcher->dispatch(UsuarioPasswordModificadoEvent::NAME, $event);
+
+        $userManager->updateUser($usuario);
+
+        $this->get('session')->getFlashBag()->add('success', 'La contraseña se ha modificado satisfactoriamente. Puede ingresar nuevamente con su nueva contraseña.');
+    }
 }
