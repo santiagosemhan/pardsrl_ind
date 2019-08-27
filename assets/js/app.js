5b943907700364777d75021cb11a7c89f2746bbb
Santiago Semhan <santiagosemhan@gmail.com>
2019-08-27 15:43:05 -0300

    add fullscreen window widget

--- assets/js/app.js
+++ assets/js/app.js
@@ -91,6 +91,38 @@ $(document).ready(function () {
     });
 
     $('.select2').select2();
+    $('*[data-widget="expand"]').click(function(){
+        // console.log(this);
+        // if already full screen; exit
+        // else go fullscreen
+        if (
+          document.fullscreenElement ||
+          document.webkitFullscreenElement ||
+          document.mozFullScreenElement ||
+          document.msFullscreenElement
+        ) {
+          if (document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+          } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+          } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+          }
+        } else {
+          element = $(this).closest('.box').get(0);
+          if (element.requestFullscreen) {
+            element.requestFullscreen();
+          } else if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+          } else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+          } else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+          }
+        }
+      });
 
     $(document).ajaxStart(function() { Pace.restart(); });
 
